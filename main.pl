9fee177a614992f996a4440bf83603324693d51e
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-05-29 18:42:37 +0200

    correction include

--- main.pl
+++ main.pl
@@ -2,10 +2,10 @@
 plateau_depart([[(11,e),(12,s),(0,w),(0,e),(0,e)],[(0,n),(0,n),(0,s),(0,w),(0,e)],[32,33,34],e]).
 %plateau_depart([[(23,n),(13,n),(0,w),(0,e),(0,e)],[(33,s),(43,s),(53,s),(0,w),(0,e)],[32,35,34],e]).
 
-include('tools.pl').
-include('affichage.pl').
-include('pousse_possible.pl').
-include('coups_possibles.pl').
+:- include('tools.pl').
+:- include('affichage.pl').
+:- include('pousse_possible.pl').
+:- include('coups_possibles.pl').
 
 
 
@@ -16,7 +16,7 @@ demande_coup(X) :-
 	coup_possible(X),
 	!.
 	
-dynamic(plateau/1).
+:- dynamic(plateau/1).
 set_plateau(P) :- retractall(plateau(_)), asserta(plateau(P)).
 affiche_plateau_courant :- plateau(X), affiche_plateau(X).
 
