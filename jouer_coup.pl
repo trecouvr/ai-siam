b109b4fef2f9fe3697e9a7dbb4955e37f852f379
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2012-06-11 13:57:01 +0200

    factorisation jouer_coup

--- jouer_coup.pl
+++ jouer_coup.pl
@@ -1,18 +1,14 @@
 
-
+update_liste([], _, []) :- !.
 update_liste([(Pi,_)|Q], (Pi,Pf,O), [(Pf,O)|Q]) :- !.
 update_liste([X|Q], C, [X|Q2]) :- update_liste(Q,C,Q2).
 
 % simple changement d'orientation
-jouer_coup([E,R,M,J], (P,P,O), [E2,R,M,J]) :- update_liste(E, (P,P,O), E2), !.
-jouer_coup([E,R,M,J], (P,P,O), [E,R2,M,J]) :- update_liste(R, (P,P,O), R2), !.
+jouer_coup([E,R,M,J], (P,P,O), [E2,R2,M,J]) :- update_liste(E, (P,P,O), E2), update_liste(R, (P,P,O), R2), !.
 
 % déplacement sur case vide
-jouer_coup([E,R,M,J], (Pi,Pf,O), [E2,R,M,J]) :-
+jouer_coup([E,R,M,J], (Pi,Pf,O), [E2,R2,M,J]) :-
 	case_vide([E,R,M,J], Pf),
 	update_liste(E, (Pi,Pf,O), E2),
-	!.
-jouer_coup([E,R,M,J], (Pi,Pf,O), [E,R2,M,J]) :-
-	case_vide([E,R,M,J], Pf),
 	update_liste(R, (Pi,Pf,O), R2),
 	!.
